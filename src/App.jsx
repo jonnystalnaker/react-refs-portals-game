46b661667039ce9ab4c7fcd1386832b098d29ff6
jonnystalnaker <steamsynthetic@gmail.com>
2024-05-17 16:11:16 -0700

    adding challenge placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge
+          title="Easy"
+          target={1}
+        />
+        <TimerChallenge
+          title="Not Easy"
+          target={5}
+        />
+        <TimerChallenge
+          title="Getting Touch"
+          target={10}
+        />
+        <TimerChallenge
+          title="Pros Only"
+          target={15}
+        />
+      </div>
     </>
   );
 }
